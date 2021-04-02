ff73fea4cd0a63a302b1a1951eabe60c285bbbd6
Etroid <mkeereman@tableau.com>
2021-04-02 10:55:34 -0700

    Correctly set options

--- src/Controller/ArgoController.php
+++ src/Controller/ArgoController.php
@@ -52,7 +52,7 @@ class ArgoController extends ControllerBase {
    */
   public function exportConfig(Request $request) {
     $langcode = $request->get('langcode');
-    $include_translations = (bool) $request->query->get('include-translations', FALSE);
+    $include_translations = intval($request->query->get('include-translations', FALSE));
     $export = $this->argoService->exportConfig($langcode, [
       'include_translations' => $include_translations,
     ]);
@@ -265,7 +265,7 @@ class ArgoController extends ControllerBase {
    */
   public function exportLocale(Request $request) {
     $langcode = $request->get('langcode');
-    $include_translations = (bool) $request->query->get('include-translations', FALSE);
+    $include_translations = intval($request->query->get('include-translations', FALSE));
     $export = $this->argoService->exportLocale($langcode, [
       'include_translations' => $include_translations,
     ]);
